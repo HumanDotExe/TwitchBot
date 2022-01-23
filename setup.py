9a56a3b19fd864fd39c7f7cb3fc6f4babed847ee
Daenara <nadine@daenara.net>
2022-01-23 16:56:12 +0100

    added setup.py (untested)

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='TwitchBot',
+    author='Daenara',
+    version='0.0.1',
+    url='https://github.com/HumanDotExe/TwitchBot',
+    packages=setuptools.find_packages(),
+    py_modules=["chat_bot", "data_types", "twitch_api", "utils", "webserver"],
+    python_requires='>=3.7, <3.9',
+    install_requires=["twitchAPI", "twitchio", "aiohttp", "PyYAML", "schema"],
+    package_data={
+        '': ['streams/default.yaml'],
+    }
+)
